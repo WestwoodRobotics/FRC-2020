b98f6a78c7c486add218995cdd9b42d4e05f9a0c
unknown <ishdeshpa@gmail.com>
2020-01-06 00:51:10 -0600

    Initial commit. Created skeleton for 2020 codebase and added RobotContainer.java and Constants.java

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+public final class Constants{
+    //Create a new "constants" class for each mech
+    public static final class DriveConstants{
+        //Include necessary constants for drivetrain.
+    }
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+public class RobotContainer{
+    
+    //Declare all auton routines and necessary subsystems here
+
+    public RobotContainer(){
+        //Configure default commands (for auton choosing), button bindings
+        configureButtonBindings();
+    }
+
+    private void configureButtonBindings(){
+        //Configure button bindings
+    }
+}
